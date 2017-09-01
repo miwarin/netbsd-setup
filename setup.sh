860aa9efed3c5319433d4d50ce793a9d7eaab2bd
Susumu Miwa <miwarin@gmail.com>
2017-09-01 23:41:23 +0900

    add gnuls

--- setup.sh
+++ setup.sh
@@ -113,16 +113,12 @@ pkg_install()
   pkgs="
   converters/nkf
   devel/bmake
-  devel/mercurial
-  devel/subversion
-  devel/scmgit
+  devel/git
   editors/vim
   lang/ruby
-  lang/python33
+  lang/python
   lang/perl5
   misc/lv
-  misc/rubygems
-  misc/screen
   mail/quickml
   mail/postfix
   net/wget
@@ -138,6 +134,7 @@ pkg_install()
   security/sudo
   shells/zsh
   www/apache24
+  misc/gnuls
   "
   
   for p in $pkgs; do
